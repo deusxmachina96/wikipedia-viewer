d506225567ff484e0788927224f4e651c51fd8bc
DeusExMachina <deusxmachina96@gmail.com>
2016-01-30 18:45:08 +0100

    Fix autocomplete bugs

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,12 @@
 (function() {
 
-    var app = angular.module("wikiApp", ["ngTouch", "ngSanitize", "filters"]);
+    var app = angular.module("wikiApp", ["ngTouch"]);
 
     app.controller("WikiSearchCtrl", ["$scope", "$http", function($scope, $http) {
         $scope.data = [];
+        $scope.autocompleteData = [];
         $scope.error = null;
+
         $scope.search = function(title) {
             console.log("called");
             $http.jsonp("https://en.wikipedia.org/w/api.php?exintro&explaintext&exsentences=1&exlimit=max", {
@@ -30,6 +32,51 @@
                 console.log('error');
             });
         };
+
+        $scope.randomSearch = function() {
+
+        	$http.jsonp("https://en.wikipedia.org/w/api.php", {
+                params: {
+                    action: "query",
+                    list: "random",
+                    rnnamespace: 0,
+                    rnlimit: 1,
+                    format: "json",
+                    callback: "JSON_CALLBACK"
+                }
+            }).then(function(response) {
+                window.open("https://en.wikipedia.org/?curid=" + response.data.query.random[0].id);
+                console.log(response);
+            }, function(response) {
+                //error
+                console.log('error');
+            });
+        }
+
+        $scope.autocomplete = function(title) {
+        	$scope.autocompleteData = [];
+        	$http.jsonp("https://en.wikipedia.org/w/api.php", {
+                params: {
+                    action: "opensearch",
+                    search: title,
+                    namespace: 0,
+                    limit: 10,
+                    format: "json",
+                    callback: "JSON_CALLBACK"
+                }
+            }).then(function(response) {
+            	for(var i = 0; i < response.data[1].length; i++) {	
+            		$scope.autocompleteData.push({
+	            		text: response.data[1][i],
+	            		link: response.data[3][i]
+	            	});
+            	}
+                console.log(response);
+            }, function(response) {
+                //error
+                console.log('error');
+            });
+        }
     }]);
 
 })();
--- js/filters.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var app = angular.module("filters", []);
-
-    app.filter("sanitize", ['$sce', function($sce) {
-        return function(htmlCode) {
-            return $sce.trustAsHtml(htmlCode);
-        }
-    }]);
-})();
